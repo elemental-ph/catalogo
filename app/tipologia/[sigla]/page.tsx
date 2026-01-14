4e1baf9fc11cb6d8c5dc778aff3358fb0c3a0913
elemental-ph <ph@elementalchile.cl>
2026-01-14 15:53:57 -0300

    actualiza descripcion de las paginas

--- app/tipologia/[sigla]/page.tsx
+++ app/tipologia/[sigla]/page.tsx
@@ -40,7 +40,7 @@ const query = `*[_type == "tipologia" && sigla == $sigla][0]{
 
   const tipologia = await client.fetch(query, { sigla });
   const postTitle = `${tipologia.sigla} - ${tipologia.name}`;
-  const postDescription = `${tipologia.descripcion}`;
+  const postDescription = `Diseño por ELEMENTAL con tecnología industrializada`;
   
   return {
     title: postTitle,
