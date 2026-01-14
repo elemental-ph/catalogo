dd2092aeebe3a8ca993eaaac8c636abe4162104a
elemental-ph <ph@elementalchile.cl>
2026-01-14 11:21:09 -0300

    corrige imagen opengraph

--- app/tipologia/[sigla]/opengraph-image.tsx
+++ app/tipologia/[sigla]/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from 'next/og';
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
-import Image from 'next/image';
 
 // Image metadata
 export const size = {
