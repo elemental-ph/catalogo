51fa0997d591a64c47ae7f2aedce963c922d8bbb
elemental-ph <ph@elementalchile.cl>
2026-08-18 12:42:15 -0400

    cambia a edge nuevamente

--- app/(main)/(con-footer)/tipologia/[sigla]/opengraph-image.tsx
+++ app/(main)/(con-footer)/tipologia/[sigla]/opengraph-image.tsx
@@ -2,7 +2,8 @@ import { ImageResponse } from 'next/og';
 
 export const size = { width: 1200, height: 630 };
 export const contentType = 'image/png';
-export const runtime = 'nodejs';
+export const runtime = 'edge'; // Recommended for performance
+export const revalidate = 3600;
 
 export default function Image() {
   return new ImageResponse(
