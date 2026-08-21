0f80158d8a984d7c39f516f307affcc6eeb5977a
elemental-ph <ph@elementalchile.cl>
2026-08-21 16:06:54 -0400

    actualiza logo y X

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed z-30 top-25 md:top-13 right-8 flex justify-end">
+    <footer className="fixed z-51 top-25 md:top-13 right-8 md:bg-transparent bg-[#505050] flex justify-end">
       <Link 
         href="/" 
         className="flex items-center font-bold decoration-3 underline-offset-6 transition duration-300 hover:underline"
       >
         
         <span className="text-2xl leading-none">volver</span>
-        <span className="text-2xl leading-none font-light">&nbsp;&#128473;</span>
+        <span className="text-2xl leading-none font-light w-6 h-6 min-w-6 min-h-6">&nbsp;&#128473;</span>
         
       </Link>
     </footer>
