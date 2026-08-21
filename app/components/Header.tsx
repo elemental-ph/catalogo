4b4c23e303ab83038a7bcbc8364a82998cbca208
elemental-ph <ph@elementalchile.cl>
2026-08-21 15:48:38 -0400

    corrige X

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Header: React.FC = () => {
 
   return (
-    <header className="fixed bg-[#505050] z-50 pointer-events-none transition-all w-full px-6 md:px-8">
+    <header className="fixed md:bg-transparent bg-[#505050] z-50 pointer-events-none transition-all w-full px-6 md:px-8">
   <div className="flex  items-center justify-center py-6 md:h-[110px] md:py-0 decoration-3 underline-offset-6 transition duration-300 hover:underline">
     <Link className='flex-column w-[200px] w-full md:w-[300px] pointer-events-auto ' href="/contacto" >
     <div className="md:relative md:right-65 md:top-6 text-xl md:h-fit md:leading-none flex font-bold justify-center">
